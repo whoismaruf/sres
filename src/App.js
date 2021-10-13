e5dab976710c9573ef80bcd66d6e243f864aacef
Maruf Khan <whoismaruf@outlook.com>
2021-10-13 22:14:50 +0600

    created route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Navbar from "./components/Common/Navbar";
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Registation from './pages/Registation';
+
+
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <BrowserRouter>
+        <Navbar></Navbar>
+        <Switch>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route exact path="/home">
+            <Home></Home>
+          </Route>
+          <Route exact path="/register">
+            <Registation></Registation>
+          </Route>
+          <Route exact path="/login">
+            <Login></Login>
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
-
+ 
 export default App;
