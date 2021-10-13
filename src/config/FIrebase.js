1e8881b4d4d61e78865393c206c537e9723e680f
Maruf Khan <whoismaruf@outlook.com>
2021-10-13 09:29:02 +0600

    replaced mui with tailwindcss

---
+++ src/config/FIrebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDrLoE4N8XcOVj7Q2NjIMkq48qipXjqnuI",
+  authDomain: "test-auth-c51c3.firebaseapp.com",
+  projectId: "test-auth-c51c3",
+  storageBucket: "test-auth-c51c3.appspot.com",
+  messagingSenderId: "357858526282",
+  appId: "1:357858526282:web:c452e8efb48fe04b1952b3"
+};
+
+// Initialize Firebase
+
+const app = () => {
+    initializeApp(firebaseConfig);
+}
+
+export default app;
